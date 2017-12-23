2fa1d05332355f372d3f418b0dd99ea4a397cd7d
qq276105859 <276105859@qq.com>
2017-12-23 23:35:10 +0800

    设计Controller

---
+++ chapter2_controller/src/main/java/com/balance/ControllerStartApplication.java
@@ -0,0 +1,14 @@
+package com.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 孔垂云 on 2017/12/23.
+ */
+@SpringBootApplication
+public class ControllerStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerStartApplication.class, args);
+    }
+}
---
+++ chapter2_controller/src/main/java/com/balance/model/User.java
@@ -0,0 +1,34 @@
+package com.balance.model;
+
+/**
+ * Created by 孔垂云 on 2017/12/23.
+ */
+public class User {
+    private int id;
+    private String username;//
+    private String passsword;//
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+}
