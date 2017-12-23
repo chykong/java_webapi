1c82215ed6e875ed548428a3af324ae909c316dd
qq276105859 <276105859@qq.com>
2017-12-23 19:57:27 +0800

    设计dao、service

---
+++ chapter1_dao/src/main/java/com/balance/DaoStartApplication.java
@@ -0,0 +1,14 @@
+package com.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 孔垂云 on 2017/12/23.
+ */
+@SpringBootApplication
+public class DaoStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DaoStartApplication.class, args);
+    }
+}
